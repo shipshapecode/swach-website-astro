417fa5a4e1bf45fede7a2575151aa5ee8d701918
Robert Wagner <rwwagner90@gmail.com>
2024-01-03 11:40:07 -0500

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    speedInsights: {
+      enabled: true,
+    },
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   site: 'https://swach.io/',
   integrations: [
     tailwind(),
